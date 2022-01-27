54057cda1c154f4a0f51b859b1bc220fa659d1fc
ryuk156 <yp15601560@gmail.com>
2022-01-27 14:01:43 +0530

    feat: api-test

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -2,11 +2,25 @@
 import dbConnect from "../../../lib/mongo_db"
 import Product from "../../../models/Product"
 
-export default function handler(req, res){
+export default async function handler(req, res){
   const {method} =req
 
   dbConnect()
 
-  if(method==="GET"){}
-  if(method==="POST"){}
+  if(method==="GET"){
+    try{
+      const products = await Product.find();
+      res.status(201).json(products)
+    }catch(err){
+        res.status(500).json(err)
+    }
+  }
+  if(method==="POST"){
+      try{
+        const product = await Product.create(req.body);
+        res.status(201).json(product)
+      }catch(err){
+          res.status(500).json(err)
+      }
+  }
 }
